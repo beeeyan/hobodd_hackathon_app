6e3f89b85de48b57aabc507ea75c774c2c8f1483
hott3 <113865871+hott3@users.noreply.github.com>
2024-11-03 04:10:15 +0900

    記念日一覧画面のUIを実装

--- lib/feature/share_calendar/presentation/anniversary_list.dart
+++ lib/feature/share_calendar/presentation/anniversary_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intersperse/intersperse.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class AnniversaryList extends StatelessWidget {
   const AnniversaryList({super.key});
@@ -7,8 +10,86 @@ class AnniversaryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('記念日一覧'),
+    return Center(
+      child: Stack(
+        children: [
+          ListView(
+            children: [
+              ...List<Widget>.generate(
+                6,
+                (index) => _AnniversaryListTile(
+                  anniversaryName: 'ルームの記念日$index.記念日名',
+                  anniversaryDate: DateTime.now(),
+                  message: 'おめでとう！',
+                ),
+              ).intersperse(const Divider()),
+            ],
+          ),
+          Positioned(
+            right: 16.w,
+            bottom: 16.w,
+            child: FloatingActionButton(
+              elevation: 0,
+              mini: true,
+              child: const Icon(Symbols.edit),
+              onPressed: () => {
+                // TODO(hott3): 記念日の作成ダイアログの表示
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AnniversaryListTile extends StatelessWidget {
+  const _AnniversaryListTile({
+    super.key,
+    required this.anniversaryName,
+    required this.anniversaryDate,
+    required this.message,
+  });
+
+  final String anniversaryName;
+  final DateTime anniversaryDate;
+  final String message;
+
+  // anniversaryDate を 'yyyy/MM/dd' にフォーマットする
+  String get formattedAnniversaryDate {
+    return '${anniversaryDate.year}/${anniversaryDate.month}/${anniversaryDate.day}';
+  }
+
+  // anniversaryDate を 'MM/dd' にフォーマットする
+  String get formattedAnniversaryDateShort {
+    return '${anniversaryDate.month}/${anniversaryDate.day}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    return ListTile(
+      leading: Container(
+        width: 46.w,
+        height: 46.w,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: colorScheme.secondaryContainer,
+        ),
+        child: Center(
+          child: Text(
+            formattedAnniversaryDateShort,
+            style: textTheme.titleMedium,
+          ),
+        ),
+      ),
+      title: Text(anniversaryName),
+      subtitle: Text('$formattedAnniversaryDate $message'),
+      onTap: () {
+        // TODO(hott3): 記念日の編集ダイアログを表示する
+      },
     );
   }
 }
