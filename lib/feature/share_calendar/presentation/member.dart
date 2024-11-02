2bfee44fb67a8a48b8bd51ef32c530eba08f7de7
hott3 <113865871+hott3@users.noreply.github.com>
2024-11-03 08:31:04 +0900

    メンバー詳細画面のUI実装

--- lib/feature/share_calendar/presentation/member.dart
+++ lib/feature/share_calendar/presentation/member.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intersperse/intersperse.dart';
+
+import '../../../util/formatter/date_time_formatter.dart';
 
 class MemberPage extends StatelessWidget {
   const MemberPage({super.key});
@@ -8,7 +12,90 @@ class MemberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO(hott3): メンバー詳細ページのUIを実装する
-    return const Text('data');
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    // TODO(hott3): メンバー名を取得して表示する
+    const memberName = 'メンバー名';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(memberName),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200.h,
+            child: Container(
+              width: 100.w,
+              height: 100.w,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: colorScheme.secondaryContainer,
+              ),
+              child: Center(
+                child: Text(
+                  memberName != '' ? memberName[0] : '',
+                  style: textTheme.displayMedium,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                ...List<Widget>.generate(
+                  6,
+                  (index) => _MemberLogListTile(
+                    logStatus: '👍',
+                    // TODO(hott3): カレンダーそのものの日時を取得する
+                    clickedDate: DateTime.now(),
+                    // TODO(hott3): カレンダーをめくった日時
+                    logCreatedDate: DateTime.now(),
+                  ),
+                ).intersperse(const Divider()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MemberLogListTile extends StatelessWidget {
+  const _MemberLogListTile({
+    super.key,
+    required this.logStatus,
+    required this.clickedDate,
+    required this.logCreatedDate,
+  });
+
+  final String logStatus;
+
+  /// カレンダーそのものの日時
+  final DateTime clickedDate;
+
+  /// カレンダーをめくった日時
+  final DateTime logCreatedDate;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return ListTile(
+      leading: SizedBox(
+        width: 52.w,
+        height: 52.w,
+        child: Center(
+          child: Text(
+            logStatus,
+            style: textTheme.displayMedium,
+          ),
+        ),
+      ),
+      title: Text(logCreatedDate.toMMddHHmmSeparatedBySlash()),
+      subtitle: Text('${clickedDate.toMMddSeparatedBySlash()} のカレンダーをめくりました'),
+    );
   }
 }
--- lib/util/formatter/date_time_formatter.dart
+++ lib/util/formatter/date_time_formatter.dart
@@ -1,6 +1,12 @@
 import 'package:intl/intl.dart';
 
 extension DateTimeFormatter on DateTime {
+  String toMMddHHmmSeparatedBySlash() {
+    final formatter = DateFormat('MM/dd HH:mm');
+    final formatted = formatter.format(this);
+    return formatted;
+  }
+
   String toMMddSeparatedBySlashLong() {
     final formatter = DateFormat('yyyy/MM/dd');
     final formatted = formatter.format(this);
