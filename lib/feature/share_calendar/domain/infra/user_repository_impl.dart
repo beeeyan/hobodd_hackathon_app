a96aff2f1939bb125a0fbd6b91545c58bb2061e5
takuro abe <abe.t@docodoor.co.jp>
2024-11-03 09:43:36 +0900

    userRepositoryを作成

---
+++ lib/feature/share_calendar/domain/infra/user_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../../api/api_client/api_client.dart';
+import '../../../../api/base_url.dart';
+import '../model/user_get_data.dart';
+
+final userRepositoryProvider = Provider<UserRepositoryImpl>(
+  (ref) => UserRepositoryImpl(
+    ref.watch(apiClientProvider),
+  ),
+);
+
+class UserRepositoryImpl {
+  UserRepositoryImpl(this.apiClient);
+
+  final ApiClient apiClient;
+
+  Future<List<UserGetData>> get({
+    required String roomId,
+  }) async {
+    final response = await apiClient.get(
+      '$baseUrl/user/:room_id',
+      queryParameters: <String, dynamic>{
+        'room_id': roomId,
+      },
+    );
+    return response.when(
+      success: (data) {
+        final items = data.main['items'] as List;
+        return items
+            .map(
+              (item) => UserGetData.fromJson(item as Map<String, dynamic>),
+            )
+            .toList();
+      },
+      failure: (error) => throw Exception(error),
+    );
+  }
+}
